59344c44a481ccb49f80746924a92335fdcac3a1
Nicoo628 <nico.moralesr2000@gmail.com>
2024-07-01 22:38:00 -0400

    me di cuenta que puse todos los archivos pero no tenian ningun contenido
    Estoy arreglando este error con todos los archivos

--- Repositorioo/ciudadano.py
+++ Repositorioo/ciudadano.py
@@ -1,2 +1,11 @@
-class Ciudadano: 
-    
+class Ciudadano: #base de atributos simples por ahora
+    def __init__(self, comunidad, _id,nombre_y_apellido, familia, emfermedad, estado):
+        self.comunidad = comunidad
+        self._id = _id
+        self.nombre_y_apellido
+        self.familia
+        
+        self.enfermedad = None
+        #probablemente esto no va a funcionar asi q despues veo como lo voy a hacer
+        self.estado = True # enfermo
+        self.estado = False #no enfermo
