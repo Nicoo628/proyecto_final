503a82c2b78c58cea9b752ca226b57ed31482ccf
Nicoo628 <nico.moralesr2000@gmail.com>
2024-07-01 23:10:59 -0400

    las bases

--- Repositorioo/comunidad.py
+++ Repositorioo/comunidad.py
@@ -1,2 +1,8 @@
 class Comunidad:
-pass
+    def __init__(self, num_ciudadanos, promedio_conexion_fisica, enfermedad, probabilidad_conexion_fisica, num_infectados):
+        self.num_ciudadanos = num_ciudadanos
+        self.promedio_conexion_fisica = promedio_conexion_fisica
+        self.enfermedad = enfermedad
+        self.probabilidad_conexion_fisica = probabilidad_conexion_fisica
+        self.num_infectados = num_infectados
+        
