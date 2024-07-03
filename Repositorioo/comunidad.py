d69ce823c0456ed408ca9d927973342a8d65968d
Nicoo628 <nico.moralesr2000@gmail.com>
2024-07-03 16:51:37 -0400

    Segundo avance 3-jul

--- Repositorioo/comunidad.py
+++ Repositorioo/comunidad.py
@@ -1,8 +1,24 @@
 class Comunidad:
-    def __init__(self, num_ciudadanos, promedio_conexion_fisica, enfermedad, probabilidad_conexion_fisica, num_infectados):
+    def __init__(self, num_ciudadanos, num_infectados, enfermedad, prob_infeccion, duracion_enfermedad):
         self.num_ciudadanos = num_ciudadanos
-        self.promedio_conexion_fisica = promedio_conexion_fisica
-        self.enfermedad = enfermedad
-        self.probabilidad_conexion_fisica = probabilidad_conexion_fisica
         self.num_infectados = num_infectados
-        
+        self.num_recuperados = 0
+        self.enfermedad = enfermedad
+        self.prob_infeccion = prob_infeccion
+        self.duracion_enfermedad = duracion_enfermedad
+        self.ciudadanos = []
+
+    def agregar_ciudadano(self, ciudadano):#append agrega ciudadano a la lista ciudadanos
+        self.ciudadanos.append(ciudadano)
+
+    def get_num_infectados(self):
+        return self.num_infectados
+
+    def set_num_infectados(self, num_infectados):
+        self.num_infectados = num_infectados
+
+    def get_num_recuperados(self):
+        return self.num_recuperados
+
+    def set_num_recuperados(self, num_recuperados):
+        self.num_recuperados = num_recuperados
