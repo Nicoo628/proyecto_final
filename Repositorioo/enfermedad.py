5337563348280273bf7a906ee74315685dd416ac
Nicoo628 <nico.moralesr2000@gmail.com>
2024-07-03 16:52:58 -0400

    avance 3-jul
    falta por implementar

--- Repositorioo/enfermedad.py
+++ Repositorioo/enfermedad.py
@@ -1,16 +1,14 @@
 class Enfermedad:
-    def __init__(self, infeccion_probable, promedio_pasos):
-        self.infeccion_probable = infeccion_probable# Probabilidad de alguien sano se infecte de forma fisica
-        self.promedio_pasos = promedio_pasos # el tiempo en el cual puede infectar antes de que se recupere o se muera
-        self.enfermo = False
-        self.contador = 0
+    def __init__(self, infeccion_probable, duracion, prob_recuperacion):
+        self.infeccion_probable = infeccion_probable
+        self.duracion = duracion
+        self.prob_recuperacion = prob_recuperacion
 
-    def infectar(self):   # aca va a ir eso de 0.3 de enfermarse y 0.8 de posibilidad 
-        self.enfermo = True
-        self.contador = self.promedio_pasos
+    def get_infeccion_probable(self):
+        return self.infeccion_probable
 
-    def paso(self):
-        if self.enfermo:
-            self.contador -= 1 # los pasos durante esta infectado el ciudadano
-            if self.contador <= 0:# valor para el numero del tiempo
-                self.enfermo = False
+    def get_duracion(self):
+        return self.duracion
+
+    def get_prob_recuperacion(self):
+        return self.prob_recuperacion
